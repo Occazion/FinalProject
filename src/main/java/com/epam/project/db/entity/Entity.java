46f3c1987d7ce52095217b0c8b29581494dcf750
dMigGXeI <C1Y_cdFdJi>
2020-10-20 00:08:30 +0300

    Added DAO tests

--- src/main/java/com/epam/project/db/entity/Entity.java
+++ src/main/java/com/epam/project/db/entity/Entity.java
@@ -5,8 +5,6 @@ import java.io.Serializable;
 /**
  * Root of all entities which have identifier field.
  *
- * @author D.Kolesnikov
- *
  */
 public abstract class Entity implements Serializable {
 
